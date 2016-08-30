46edc8d09d7bfab211f4ebaffd0f8f24c23d8358
Peng Li <seudut@gmail.com>
2016-08-30 23:11:49 +0800

    server

--- c-cpp/socket/server.c
+++ c-cpp/socket/server.c
@@ -10,12 +10,24 @@
 int main ()
 {
   struct sockaddr_in sockfd;
+  struct sockaddr_in name;
+  struct sockaddr_in clien_name;
 
   sockfd = socket(PF_INET, SOCK_STREAM, 0);
   assert(sockfd > -1);
-assert(bind())
+
+  assert (bind(sockfd, (struct sockaddr *)&name, sizeof(name)) > -1);
+
+  listen(sockfd, 5);
+
+  while (1) {
+    accept(sockfd, (struct sockaddr *)&client_name, &client_name_len);
+  }
 
 
 
+  
+  
+  
   return 0;
 }
